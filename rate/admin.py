d18a4659c27815cc476b3e1fc527f7f97cd7ec8e
Victor Quach <varaal@gmail.com>
2017-04-09 05:48:37 +0200

    Add fields

--- rate/admin.py
+++ rate/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from rate.models import Class, Department, Year, Period, Type
 
 # Register your models here.
+admin.site.register(Department)
+admin.site.register(Year)
+admin.site.register(Type)
+admin.site.register(Period)
+admin.site.register(Class)
+
--- rate/models.py
+++ rate/models.py
@@ -1,3 +1,37 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Department(models.Model):
+    trigram = models.CharField(max_length=254)
+    name = models.CharField(max_length=254)
+
+
+class Year(models.Model):
+    name = models.CharField(max_length=254)
+
+
+class Period(models.Model):
+    name = models.CharField(max_length=254)
+
+
+class Type(models.Model):
+    name = models.CharField(max_length=254)
+
+
+class Class(models.Model):
+    name = models.CharField(max_length=254)
+    title = models.CharField(max_length=254)
+    professor = models.CharField(max_length=254)
+    description = models.TextField()
+    department = models.ForeignKey('Department', on_delete=models.CASCADE)
+    year = models.ForeignKey('year', on_delete=models.CASCADE)
+    period = models.ForeignKey('period', on_delete=models.CASCADE)
+    type = models.ForeignKey('type', on_delete=models.CASCADE)
+    has_pale = models.NullBooleanField()
+    has_project = models.NullBooleanField()
+    has_presentation = models.NullBooleanField()
+    has_oral = models.NullBooleanField()
+
+
