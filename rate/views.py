8ccd10196836dc5ddd5c745ec29d1089f5b74788
Victor Quach <varaal@gmail.com>
2017-04-21 23:10:03 +0200

    Remove useless annotations
    By default, all pages require login as defined in middleware.py

--- rate/views.py
+++ rate/views.py
@@ -5,7 +5,6 @@ from rate.models import Student, Course, Assessment
 import json
 
 
-@login_required
 def home(request):
     attributes = request.session['attributes']
     try:
@@ -22,14 +21,12 @@ def home(request):
     return render(request, 'rate/home.html', {'student': student})
 
 
-@login_required
 def course(request, code):
     course = get_object_or_404(Course, code=code)
     assessments = Assessment.objects.filter(course = course)
     return render(request, 'rate/course.html', {'course': course, 'assessments': assessments})
 
 
-@login_required
 def all_courses(request):
     courses = Course.objects.all()
     return render(request, 'rate/all_courses.html', {'courses': courses})
